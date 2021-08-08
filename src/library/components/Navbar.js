0ac9223153f9b2be1175e652296ef9b9f276c722
Namdev Jadhav <namdevjadhav91@gmail.com>
2021-08-08 20:24:46 +0530

    nav bar style done

--- src/library/components/Navbar.js
+++ src/library/components/Navbar.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 
+const useStyles = makeStyles((theme) => ({
+    navCont: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+    },
+    navitem: {
+        padding: 12,
+        color: 'white',
+        textDecoration: 'none',
+        margin: 8,
+        backgroundColor: theme.palette.secondary.main,
+    },
+}))
 const Navbar = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/signin">SignIn</Link>
-            <Link to="/signup">SignUp</Link>
-            {/* <Link to="/profile">Profile</Link> */}
-            <Link to="/counter">Counter</Link>
+        <div className={classes.navCont}>
+            <Link className={classes.navitem} to="/">Home</Link>
+            <Link className={classes.navitem} to="/signin">SignIn</Link>
+            <Link className={classes.navitem} to="/signup">SignUp</Link>
+            {/* <Link className={classes.navitem} to="/profile">Profile</Link> */}
+            <Link className={classes.navitem} to="/counter">Counter</Link>
         </div>
     );
 };
